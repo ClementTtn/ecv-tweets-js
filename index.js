e714a6fca413344aa39890f59556e305b6122066
Clément Tutin <clement.tutin.pro@gmail.com>
2023-11-30 13:55:28 +0100

    Question 3

--- index.js
+++ index.js
@@ -33,26 +33,24 @@ const tweets = [
 
 const tweetsList = document.getElementById('tweets-list');
 
-// Mise en place d'une boucle forEach sur le tableau tweets
-// tweet représente l'objet parcouru dans le tableau par la boucle
 tweets.forEach(tweet => {
   const liTweet = document.createElement('li');
   liTweet.classList.add('tweet');
 
   const pContent = document.createElement('p');
   pContent.classList.add('content');
-  // firstTweet.content -> tweet.content ('content' de l'objet parcouru)
   pContent.textContent = 'Contenu du tweet : ' + tweet.content;
 
   const pLikes = document.createElement('p');
   pLikes.classList.add('likes');
-  // firstTweet.likes -> tweet.likes ('likes' de l'objet parcouru)
   pLikes.textContent = 'Nombre de likes : ' + tweet.likes;
-
+  // Si le tweet possède plus de 10 likes, ajout de la classe 'favorite'
+  if (tweet.likes > 10) {
+    liTweet.classList.add('favorite');
+  }
 
   const pAuthor = document.createElement('p');
   pAuthor.classList.add('author');
-  // firstTweet.author -> tweet.author ('author' de l'objet parcouru)
   pAuthor.textContent = 'Auteur : ' + tweet.author;
 
   liTweet.appendChild(pContent);
